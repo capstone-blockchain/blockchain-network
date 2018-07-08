bcf548dbca4b3685d6def53d296f4a9eefd0a5e5
joebui <dienbuijoe@outlook.com>
2018-07-08 09:35:32 +0700

    reorganize code

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 const mqtt = require("mqtt")
+global.mqttClient = mqtt.connect(`mqtt://${process.env.MQTT_BROKER_IP}`)
 // const WebSocket = require("ws")
-
 const BlockChain = require("./src/block-chain")
+const features = new BlockChain()
 const server = require("./src/server")
 require("./src/mongodb/connection")
 const BlockModel = require("./src/mongodb/block")
-const topics = require("./mqtt-topics")
-global.mqttClient = mqtt.connect(`mqtt://${process.env.MQTT_BROKER_IP}`)
-
-const features = new BlockChain()
-const mqttClient = global.mqttClient
+const mqttSubcription = require("./src/mqtt-subscriptions")
 
 // Node list
 global.nodes = []
@@ -40,53 +37,5 @@ BlockModel.deleteMany({}).then(() => {
 //   })
 // })
 
-mqttClient.on("connect", () => {
-  mqttClient.subscribe(topics.REQUEST_BLOCKCHAIN)
-  mqttClient.subscribe(topics.BROADCAST_BLOCKCHAIN)
-  mqttClient.subscribe(topics.REQUEST_LATEST_BLOCK)
-  mqttClient.publish(topics.REQUEST_BLOCKCHAIN, process.env.NODE_IP)
-})
-
-mqttClient.on("message", async (topic, message) => {
-  let blockchain
-
-  switch (topic) {
-    case topics.REQUEST_BLOCKCHAIN:
-      if (process.env.NODE_IP === message.toString()) break
-      require("debug")("REQUEST_BLOCKCHAIN")(message.toString())
-      blockchain = await features.getBlockchain()
-      mqttClient.publish(
-        topics.BROADCAST_BLOCKCHAIN,
-        JSON.stringify(blockchain)
-      )
-      break
-    case topics.BROADCAST_BLOCKCHAIN:
-      blockchain = JSON.parse(message.toString())
-      require("debug")("BROADCAST_BLOCKCHAIN")(blockchain)
-      blockchain = await features.replaceBlockChain(blockchain)
-      // Receive blockchain
-      if (blockchain) {
-        BlockModel.deleteMany({}).then(() => {
-          BlockModel.create(blockchain)
-        })
-      }
-      break
-    case topics.REQUEST_LATEST_BLOCK:
-      const block = await features.getLatestBlock()
-      require("debug")("REQUEST_LATEST_BLOCK")(block)
-      mqttClient.publish(topics.RESPONSE_LATEST_BLOCK, JSON.stringify(block))
-      break
-    case topics.RESPONSE_NEW_BLOCK:
-      const latestBlock = await features.getLatestBlock()
-      const newBlock = JSON.parse(message.toString())
-      if (features.isValidNewBlock(newBlock, latestBlock)) {
-        require("debug")("RESPONSE_NEW_BLOCK")(block)
-        BlockModel.create(newBlock)
-      }
-      break
-    default:
-      break
-  }
-})
-
+mqttSubcription()
 server()
---
+++ src/mqtt-subscriptions.js
@@ -0,0 +1,61 @@
+const BlockChain = require("./block-chain")
+const topics = require("./mqtt-topics")
+const BlockModel = require("./mongodb/block")
+
+const features = new BlockChain()
+const mqttClient = global.mqttClient
+
+module.exports = () => {
+  mqttClient.on("connect", () => {
+    mqttClient.subscribe(topics.REQUEST_BLOCKCHAIN)
+    mqttClient.subscribe(topics.BROADCAST_BLOCKCHAIN)
+    mqttClient.subscribe(topics.REQUEST_LATEST_BLOCK)
+    mqttClient.publish(topics.REQUEST_BLOCKCHAIN, process.env.NODE_IP)
+  })
+
+  mqttClient.on("message", async (topic, message) => {
+    let blockchain
+
+    switch (topic) {
+      case topics.REQUEST_BLOCKCHAIN:
+        if (process.env.NODE_IP === message.toString()) break
+        require("debug")("REQUEST_BLOCKCHAIN")(message.toString())
+        blockchain = await features.getBlockchain()
+        mqttClient.publish(
+          topics.BROADCAST_BLOCKCHAIN,
+          JSON.stringify(blockchain)
+        )
+        break
+
+      case topics.BROADCAST_BLOCKCHAIN:
+        blockchain = JSON.parse(message.toString())
+        require("debug")("BROADCAST_BLOCKCHAIN")(blockchain)
+        blockchain = await features.replaceBlockChain(blockchain)
+        // Receive blockchain
+        if (blockchain) {
+          BlockModel.deleteMany({}).then(() => {
+            BlockModel.create(blockchain)
+          })
+        }
+        break
+
+      case topics.REQUEST_LATEST_BLOCK:
+        const block = await features.getLatestBlock()
+        require("debug")("REQUEST_LATEST_BLOCK")(block)
+        mqttClient.publish(topics.RESPONSE_LATEST_BLOCK, JSON.stringify(block))
+        break
+
+      case topics.RESPONSE_NEW_BLOCK:
+        const latestBlock = await features.getLatestBlock()
+        const newBlock = JSON.parse(message.toString())
+        if (features.isValidNewBlock(newBlock, latestBlock)) {
+          require("debug")("RESPONSE_NEW_BLOCK")(block)
+          BlockModel.create(newBlock)
+        }
+        break
+
+      default:
+        break
+    }
+  })
+}
